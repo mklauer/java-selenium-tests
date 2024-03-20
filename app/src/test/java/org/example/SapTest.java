c63a9c8427f927f2647f373139256aed07242e19
D032297 <matthias.klauer@sap.com>
2024-03-20 14:50:09 +0100

    Test locally and remotely

--- app/src/test/java/org/example/SapTest.java
+++ app/src/test/java/org/example/SapTest.java
@@ -14,31 +14,47 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class SapTest {
-    private WebDriver driver;
+    private WebDriver webDriver;
+    private WebDriver remoteWebDriver;
     private NetworkListener networkListener;
+    private NetworkListener remoteNetworkListener;
 
     @BeforeClass
     public void setUp() throws MalformedURLException {
         ChromeOptions options = new ChromeOptions();
         String server = "10.237.117.237";
-        // String server = "selenium-runner.tcc.c.eu-de-2.cloud.sap"
-        //driver = new RemoteWebDriver(new URL("http://btpuser:xyz1234@" + server + ":4444"), options);
-        driver = new ChromeDriver(options);
-        networkListener = new NetworkListener(driver, "har.har");
+        // String server = "localhost";
+        // String server = "selenium-runner.tcc.c.eu-de-2.cloud.sap";
+        String credentials = "btpuser:xyz1234@";
+        remoteWebDriver = new RemoteWebDriver(new URL("http://" + credentials + server + ":4444"), options);
+        remoteNetworkListener = new NetworkListener(remoteWebDriver, "remote_har.har");
+        
+        webDriver = new ChromeDriver(options);
+        networkListener = new NetworkListener(webDriver, "har.har");
     }
 
     @Test
     public void testSapHomePage() {
         networkListener.start();
-        driver.get("https://www.sap.com");
-        String title = driver.getTitle();
+        webDriver.get("https://www.sap.com");
+        String title = webDriver.getTitle();
+        Assert.assertTrue(title.contains("SAP"), "Title does not contain 'SAP': " + title);
+    }
+
+    @Test
+    public void testSapHomePageRemote() {
+        remoteNetworkListener.start();
+        remoteWebDriver.get("https://www.sap.com");
+        String title = remoteWebDriver.getTitle();
         Assert.assertTrue(title.contains("SAP"), "Title does not contain 'SAP': " + title);
     }
 
     @AfterClass
     public void tearDown() {
-        if (driver != null) {
-            driver.quit();
+        if (webDriver != null) {
+            webDriver.quit();
+        }
+        if (networkListener != null) {
             networkListener.createHarFile();
         }
     }
